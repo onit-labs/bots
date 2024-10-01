cee3bff7bf41621c2c0ab3e99fc7c65975457ddb
Peter Ferguson <peterferguson95@gmail.com>
2024-10-01 10:29:38 +0100

    chore: remove uuid

--- src/lib/uuid.ts
+++
@@ -1,5 +0,0 @@
-import { uuidv7 } from "uuidv7";
-import type { Uuidv7 } from "../lib/validators";
-
-export const generateUuid7 = (...args: Parameters<typeof uuidv7>) =>
-	uuidv7(...args) as Uuidv7;
